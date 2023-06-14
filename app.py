3016f2a096ba96610823dda0df8e71a8ae658f06
yujik21 <yuji200221@gmail.com>
2023-06-14 00:26:39 -0700

    added

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 import streamlit as st
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
+web_apps = st.sidebar.selectbox("Select Web Apps",
+                                ("Exploratory Data Analysis", "Distributions", "Correlation"))
 
-st. title('title of your application')
-st.markdown('for example here is a **bold text**')
+df = None  # Define df variable
 
+if web_apps == "Exploratory Data Analysis":
 
-st.sidebar.title('title of the sidebar')
+    uploaded_file = st.sidebar.file_uploader("Choose a file")
 
-agree = st.checkbox('I agree')
-disagree = st.checkbox("I disagree")
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        df = pd.read_csv(uploaded_file)
+        show_df = st.checkbox("Show Data Frame", key="disabled")
 
-if agree:
-    st.write('Great!')
-if disagree:
-    st.write('Bruhhhhhhh')
+        if show_df:
+            st.write(df)
 
-side_check = st.sidebar.checkbox('I agree yes yes')
-if side_check:
-    st.write('Sidebar checkbox has been clicked')
+        num_rows = df.shape[0]
+        num_columns = df.shape[1]
+        num_categorical = len(df.select_dtypes(include=['object']).columns)
+        num_numerical = len(df.select_dtypes(
+            include=['int64', 'float64']).columns)
+        num_boolean = len(df.select_dtypes(include=['bool']).columns)
+
+        st.subheader("Dataset Statistics")
+        st.write("Number of rows:", num_rows)
+        st.write("Number of columns:", num_columns)
+        st.write("Number of categorical variables:", num_categorical)
+        st.write("Number of numerical variables:", num_numerical)
+        st.write("Number of boolean variables:", num_boolean)
+
+        selected_column = st.sidebar.selectbox("Select a Column", df.columns)
