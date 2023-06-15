e8cc1c9df539ecd788920058dd7caa4b288f9ce1
yujik21 <yuji200221@gmail.com>
2023-06-14 23:12:28 -0700

    check

--- app.py
+++ app.py
@@ -77,6 +77,67 @@ if web_apps == "Exploratory Data Analysis":
             ax.set_ylabel("Count")
             st.pyplot(fig)
 
+elif web_apps == "Distribution":
+    uploaded_file = st.sidebar.file_uploader("Choose a file")
+    df = None  # Initialize df as None
+
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        show_df = st.checkbox("Show Data Frame", key="disabled")
+
+        if show_df:
+            st.write(df)
+
+        num_rows = df.shape[0]
+        num_columns = df.shape[1]
+        num_categorical = len(df.select_dtypes(include=['object']).columns)
+        num_numerical = len(df.select_dtypes(include=['int64', 'float64']).columns)
+        num_boolean = len(df.select_dtypes(include=['bool']).columns)
+
+        st.subheader("Dataset Statistics")
+        st.write("Number of rows:", num_rows)
+        st.write("Number of columns:", num_columns)
+        st.write("Number of categorical variables:", num_categorical)
+        st.write("Number of numerical variables:", num_numerical)
+        st.write("Number of boolean variables:", num_boolean)
+
+        selected_column = st.sidebar.selectbox("Select a Column", df.columns)
+
+        # for the histogram
+        choose_color = st.color_picker('Pick a Color', "#69b3a2")
+        choose_opacity = st.slider('Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
+
+        column_type = df[selected_column].dtype
+        if column_type in ['int64', 'float64']:
+            st.subheader("Numerical Column Analysis: " + selected_column)
+
+            summary_table = df[selected_column].describe()
+            st.write("Five Number Summary:")
+            st.write(summary_table)
+
+            st.write("Histogram:")
+            fig, ax = plt.subplots()
+            sns.histplot(data=df, x=selected_column, kde=True, color=choose_color, alpha=choose_opacity)
+            ax.set_xlabel(selected_column)
+            ax.set_ylabel("Count")
+            st.pyplot(fig)
+
+        elif column_type == 'object':
+            st.subheader("Categorical Column Analysis: " + selected_column)
+
+            category_proportions = df[selected_column].value_counts(normalize=True)
+            st.write("Proportions of Each Category Level:")
+            st.write(category_proportions)
+
+            st.write("Histogram:")
+            fig, ax = plt.subplots()
+            sns.histplot(data=df, x=selected_column, color=choose_color, alpha=choose_opacity, discrete=True)
+            ax.set_xlabel(selected_column)
+            ax.set_ylabel("Count")
+            st.pyplot(fig)
+
+
+
 
 elif web_apps == "Correlation":
     if df is None:
